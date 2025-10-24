75dca9c361c9423714f0b755e44d9ce5feb0f1a7
Ben Burris <mburris1914@gmail.com>
2025-10-23 21:01:28 -0400

    final work

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>CS 330L</h1>
-      <h2>Section - 011</h2>
-      <p>WVU ID: 800427508</p>
-      <p>Hi, I am Ben Burris</p>
+      <div id="text_container">
+        <h1>CS 330L</h1>
+        <h2>Section - 011</h2>
+        <p>WVU ID: 800427508</p>
+        <p>Hi, I am Ben Burris</p>
+      </div>
       <Card />
     </div>
   );
